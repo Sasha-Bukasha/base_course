11b12789286a46c2968230006186acc909e2631d
Sasha-Bukasha <sasha.mclakoff@gmail.com>
2025-11-08 13:51:08 +0000

    процесс решения идёт

--- Task_2.py
+++ Task_2.py
@@ -2,5 +2,5 @@ a = int(input('Давай первый член: '))
 b = int(input('Давай знаменателЪ: '))
 c = int(input('Давай кол-во членов: '))
 
-for i in c:
-    print()
+for a in range(a, c + 1, b):
+    print(a)
---
+++ Task_4.py
@@ -0,0 +1,4 @@
+a = int(input('Давай число: '))
+
+for a in range(a, a + a[a], 6):
+    print(a)
