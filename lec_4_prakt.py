a670050ff31b63f62372871f1e9a6c6bea28cfff
Sasha-Bukasha <sasha.mclakoff@gmail.com>
2025-12-12 23:17:29 +0000

    я сделяль

--- lec_4_prakt.py
+++ lec_4_prakt.py
@@ -7,7 +7,7 @@
     return s / len(args)
 
 S = sr_arifmet(1, 5, 7, 29)
-print(S)"""
+print(S)
 
 import numpy as np
 
@@ -19,13 +19,13 @@ F = parabola(-1, 1, 10)
 print(F)
 
 
-"""def figure (n):
+def figure (n):
     deg = (180 - (n - 2)/ n)
     triang = (n - 2)
     circ = 
     return print(deg, triang, circ)
 
-figure()"""
+figure()
 
 def area_calculator(figure, *kwargs):
     if figure == 'circle':
@@ -33,4 +33,20 @@ def area_calculator(figure, *kwargs):
     elif figure == 'triangle':
         area = 0.5 * kwargs['l']*kwargs['h']
     else:
-        area = 0.5 * kwargs['l']*kwargs['h']
+        area = 0.5 * kwargs['l']*kwargs['h']"""
+
+
+def stepen (a, n):
+    if n == 1:
+        x = a
+    else:
+        x = a
+        i = 1
+        while i < n:
+            x = x * a
+            i = i + 1
+
+    return x
+
+S = stepen(3, 4)
+print(S)
